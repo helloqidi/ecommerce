b858832527777dade4a1022ae90a4bd17ab9ac6a
helloqidi <helloqidi@gmail.com>
2015-03-26 16:41:28 +0800

    add pic upload

--- views/add_item.php
+++ views/add_item.php
@@ -13,17 +13,28 @@
 				<textarea name="description" rows="10" cols="50"></textarea>
 			</p>
 			<p>
-				价格：<br />
-				<input type="text" name="price" />
+				价格：<br /> <input type="text" name="price" />
 			</p>
 			<p>
-				库存：<br />
-				<input type="text" name="quantity" />
+				库存：<br /> <input type="text" name="quantity" />
 			</p>
 			<p>
 				<input type="submit" value="保存" />
 			</p>
 		</form>
+
+		<div class="demo">
+			<p>说明：示例中只允许上传gif/jpg格式的图片，图片大小不能超过500k。</p>
+			<div class="btn">
+				<span>添加附件</span> <input id="fileupload" type="file" name="mypic">
+			</div>
+			<div class="progress">
+				<span class="bar"></span><span class="percent">0%</span>
+			</div>
+			<div class="files"></div>
+			<div id="showimg"></div>
+		</div>
+		
 	</div>
 
 
--- views/header.php
+++ views/header.php
@@ -7,11 +7,26 @@
 ul {
 	list-style: none;
 }
+
+/*ajax上传图片的样式 start*/
+.demo{width:620px; margin:30px auto}
+.demo p{line-height:32px}
+.btn{position: relative;overflow: hidden;margin-right: 4px;display:inline-block;*display:inline;padding:4px 10px 4px;font-size:14px;line-height:18px;*line-height:20px;color:#fff;text-align:center;vertical-align:middle;cursor:pointer;background-color:#5bb75b;border:1px solid #cccccc;border-color:#e6e6e6 #e6e6e6 #bfbfbf;border-bottom-color:#b3b3b3;-webkit-border-radius:4px;-moz-border-radius:4px;border-radius:4px;}
+.btn input {position: absolute;top: 0; right: 0;margin: 0;border: solid transparent;opacity: 0;filter:alpha(opacity=0); cursor: pointer;}
+.progress { position:relative; margin-left:100px; margin-top:-24px; width:200px;padding: 1px; border-radius:3px; display:none}
+.bar {background-color: green; display:block; width:0%; height:20px; border-radius: 3px; }
+.percent { position:absolute; height:20px; display:inline-block; top:3px; left:2%; color:#fff }
+.files{height:22px; line-height:22px; margin:10px 0}
+.delimg{margin-left:20px; color:#090; cursor:pointer}
+/*ajax上传图片的样式 end*/
+
 </style>
 
 <script type="text/javascript" src="js/jquery-1.11.2.min.js"></script>
+<script type="text/javascript" src="js/jquery.form.js"></script>
 <script type="text/javascript">
 $(function(){
+	//首页ajax删除商品功能
 	$("a.del").on("click", function(){
 		var href = $(this).attr("data-href");
 		var $parentLi = $(this).parent();
@@ -21,6 +36,60 @@ $(function(){
 			$parentLi.remove();
 		},'json');
 	});
+
+	//ajax上传图片
+	var bar = $('.bar');
+	var percent = $('.percent');
+	var showimg = $('#showimg');
+	var progress = $(".progress");
+	var files = $(".files");
+	var btn = $(".btn span");
+	$("#fileupload").wrap("<form id='myupload' action='photo.php?act=add' method='post' enctype='multipart/form-data'></form>");
+    $("#fileupload").change(function(){
+		$("#myupload").ajaxSubmit({
+			dataType:  'json',
+			beforeSend: function() {
+        		showimg.empty();
+				progress.show();
+        		var percentVal = '0%';
+        		bar.width(percentVal);
+        		percent.html(percentVal);
+				btn.html("上传中...");
+    		},
+    		uploadProgress: function(event, position, total, percentComplete) {
+        		var percentVal = percentComplete + '%';
+        		bar.width(percentVal);
+        		percent.html(percentVal);
+    		},
+			success: function(data) {
+				files.html("<b>"+data.name+"("+data.size+"k)</b> <span class='delimg' rel='"+data.pic+"'>删除</span>");
+				var img = "http://localhost/ecommerce/uploads/"+data.pic;
+				showimg.html("<img src='"+img+"'>");
+				btn.html("添加附件");
+			},
+			error:function(xhr){
+				btn.html("上传失败");
+				bar.width('0')
+				files.html(xhr.responseText);
+			}
+		});
+	});
+	
+	//ajax上传图片后的删除
+    $(".demo").delegate(".delimg","click",function(){
+// 	$(".delimg").live('click', function(){
+		var pic = $(this).attr("rel");
+		$.post("photo.php?act=delimg",{imagename:pic},function(msg){
+			if(msg==1){
+				files.html("删除成功.");
+				showimg.empty();
+				progress.hide();
+			}else{
+				alert(msg);
+			}
+		});
+	});	
+	
 });
 </script>
 </head>
